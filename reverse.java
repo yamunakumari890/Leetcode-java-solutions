8e33b8285cdd75afa09063c8e5ca2faca9d2dc18
Yamuna codes <yamunakumari731@gmail.com>
2025-07-26 15:07:25 +0530

    Add files via upload
    "Added solution for Reverse Words"

---
+++ reverse.java
@@ -0,0 +1,19 @@
+package Strings;
+
+class Solution {
+    public String reverseWords(String s) {
+     String[] words = s.trim().split("\\s+") ;
+     StringBuilder reversed = new StringBuilder();
+     for (int i = words.length-1; i >= 0; i--) {
+        reversed.append(words[i]);
+        if (i != 0) {
+            reversed.append(" ");
+        }
+     }
+     return reversed.toString();
+    }
+    public static void main(String[] args) {
+      Solution sol = new Solution();
+        System.out.println(sol.reverseWords("  girls beautifull is a yamuna "));
+    }
+}
