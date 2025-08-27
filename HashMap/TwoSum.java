e8f02ee7fbed51a0b985e5ac427892c711a02d13
yamuna gupta <yamuna123@email.com>
2025-08-27 11:44:32 +0530

    Added TwoSum.java

---
+++ HashMap/TwoSum.java
@@ -0,0 +1,26 @@
+package HashMap;
+
+import java.util.*;
+
+public class TwoSum {
+  public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for(int i=0; i<nums.length; i++){
+      int complement = target - nums[i];
+      if (map.containsKey(complement)) {
+        return new int[] { map.get(complement), i};
+      }
+      map.put(nums[i], i);
+    }
+    return new int[] {};
+  }
+  public static void main(String[] args) {
+    TwoSum obj = new TwoSum();
+    int[] nums = {2, 7, 11, 15};
+    int target = 0;
+    int ans[] = obj.twoSum(nums, target);
+
+    System.out.println(Arrays.toString(ans));
+  }
+  
+}
